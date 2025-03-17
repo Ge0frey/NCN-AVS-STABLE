a0b59dadcafb98391ac4a69c4714025d1ea29607
Ge0frey <jeffkimzyjoe@gmail.com>
2025-03-17 04:47:39 +0300

    fix: error: elf error: elf error: failed to parse elf file: section or symbol name `.data._zn13solan` is longer than `16` bytes there was a problem deploying: output { status: exitstatus(unix_wait_status(256)), stdout: "", stderr: "" }.

--- blockchain-logic-stablefunds/programs/stablefunds/src/instructions/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod create_stablecoin;
-pub mod deposit_collateral;
-pub mod mint_stablecoin;
-pub mod fetch_stablebonds;
-
-pub use create_stablecoin::*;
-pub use deposit_collateral::*;
-pub use mint_stablecoin::*;
-pub use fetch_stablebonds::*; 
